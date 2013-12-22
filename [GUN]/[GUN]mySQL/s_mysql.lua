d2a0e6797625d9c9743f4446dc5ef8c2583d5607
divx92 <divx92@gmail.com>
2013-12-22 16:33:52 +0100

    [mySQL]#6
    IPB:loginPlayer (login, pass)
    return bool

--- [GUN]/[GUN]mySQL/s_mysql.lua
+++ [GUN]/[GUN]mySQL/s_mysql.lua
@@ -11,6 +11,21 @@
 	end
 	mySQL_connect()
 
+	function IPB:loginPlayer(login, pass)
+		if IPB["connection"] and login then
+			local user = dbQuery( IPB["connection"], "SELECT member_id FROM forum_members WHERE members_l_username = '"..login.."' AND members_pass_hash= MD5(CONCAT(MD5(members_pass_salt),'', MD5( '".. pass .. "' )))")	
+			local result, num_affected_rows, last_insert_id = dbPoll( user, -1 )
+			if #result > 0 then
+				return true
+			else
+				return false
+			end
+		else
+			outputDebugString ("mySQL connection or user data missing",1 )
+			return false
+		end
+	end
+	
 	function IPB:isUser(user)
 		if IPB["connection"] and user then
 			local user = dbQuery( IPB["connection"], "SELECT member_id FROM forum_members WHERE members_l_username = '"..getPlayerName (user).."'")	
@@ -30,9 +45,9 @@
 		end
 	end
 	
-	function IPB:getUserData(UID, data)
-		if IPB["connection"] then
-				local data = dbQuery( IPB["connection"], "SELECT "..data.." FROM forum_members WHERE member_id = '"..UID.."'")	
+	function IPB:getUserData(UID, column)
+		if IPB["connection"] and UID and column then
+				local data = dbQuery( IPB["connection"], "SELECT "..column.." FROM forum_members WHERE member_id = '"..UID.."'")	
 				local result, num_affected_rows, last_insert_id = dbPoll( data, -1 )
 				if result then
 				for k,table in pairs (result) do
